b5064b79ba6056cb7e22d19cb59d6cde538693f4
Phùng Tiến Đạt <96523185+Puda14@users.noreply.github.com>
2023-04-08 13:01:05 +0700

    upload code

---
+++ lab4/Assignment1.asm
@@ -0,0 +1,21 @@
+# Laboratory Exercise 4, Assignment 1
+# Author: Phung Tien Dat - 20210163
+.text
+addi $s1, $zero, 0x7FFFFFFF # 
+addi $s2, $zero, 0x00000001 # 
+start:
+	li $t0,0			#No Overflow is default status
+	addu $s3,$s1,$s2	# s3 = s1 + s2
+	xor $t1,$s1,$s2	#Test if $s1 and $s2 have the same sign
+	bltz $t1,EXIT		#If not, exit
+	slt $t2,$s3,$s1
+	bltz $s1,NEGATIVE	#Test if $s1 and $s2 is negative?
+	beq $t2,$zero,EXIT	#s1 and $s2 are positive
+					# if $s3 > $s1 then the result is not overflow
+	j OVERFLOW
+NEGATIVE:
+	bne $t2,$zero,EXIT	#s1 and $s2 are negative
+					# if $s3 < $s1 then the result is not overflow
+OVERFLOW:
+	li $t0,1 #the result is overflow
+EXIT:
